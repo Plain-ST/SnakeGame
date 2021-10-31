58aab4ae76542a26411a90e842e0766c73515337
Plain-ST <Plain.11.dc@gmail.com>
2021-10-31 18:20:52 +0900

    変数の追加

--- js/init.js
+++ js/init.js
@@ -1,8 +1,12 @@
+//キーが押された時の処理
 document.addEventListener('keydown', keyPush);
 
+//キャンバスの要素を取得
 const canv = document.getElementById("canvas");
 const ctx = canv.getContext("2d");
+
 const SIZE = 20;
+//
 const FPS = 15;
 const MIN = 5;
 
@@ -12,8 +16,17 @@ let snake = [];
 let tail = MIN;
 let appleX = appleY = 15;
 
+let text;
+let textWidth;
+let textHeight;
+
+//snakeの色
 let color = ["#ed0000","#eff700","#07b34c","#1100ff","#ff00dd"];
 let colorCount = 0;
 
+let score = 0;
+let highscore = 0;
+
 let timer;
+//spaceキーが押されたときのフラグ
 let spaceflg = 0;
